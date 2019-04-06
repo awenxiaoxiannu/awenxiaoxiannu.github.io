24d6c079c22d1b38464a24c988ea0ffb04abc517
shitao <133397418@qq.com>
2019-04-06 09:58:24 +0800

    阿雯小仙女生日快乐

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName = document.getElementById("userName").value;
 	var pwd = document.getElementById("pwd").value;
 	//修改密码请改此处
-	if (userName == "叶婧雯" && pwd == "dfdf4456yg((*^$$$$$dsdsd7vv  fdfs") {
+    if (userName == "叶婧雯" && pwd == "258080") {
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
@@ -14,7 +14,7 @@ $('#login-button').click(function (event) {
 		if (userName != "叶婧雯") {
 			alert("姓名错误！这好像不是为你准备的");
 		} else {
-			alert("密码错误！\n提示：你今天不能问我关于这个的问题");
+            alert("密码错误！\n提示：我手机密码");
 		}
 	}
 });
